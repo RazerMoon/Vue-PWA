af8c8c734f75f5a49ffd6a3e776c5b908398d6c4
RazerMoon <33849459+RazerMoon@users.noreply.github.com>
2021-02-03 21:47:04 +0000

    pwa fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,35 @@
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('vite').UserConfig} */
 export default {
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    manifest: {
+      theme_color: "#4DBA87",
+      icons: [
+        {
+          src: "./img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          "purpose": "maskable"
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          "purpose": "maskable"
+        }
+      ],
+    }
+  })],
 }
